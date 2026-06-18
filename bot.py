0475f76c7b4947d9c6f21da9eacb2c649f5fbcaa
staceyopti <andreyzin250611@gmail.com>
2026-06-18 17:02:34 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 DATA_FILE = "codes.json"
 
-# Функция для загрузки кодов
 def load_codes():
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE, "r") as f:
@@ -17,11 +16,11 @@ def load_codes():
 def check_code():
     code = request.args.get('code')
     codes = load_codes()
-    # Если код есть в списке - возвращаем успех
     if code in codes:
         return jsonify({"valid": True})
     return jsonify({"valid": False})
 
 if __name__ == "__main__":
+    # Render сам выдает порт через переменную окружения
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
